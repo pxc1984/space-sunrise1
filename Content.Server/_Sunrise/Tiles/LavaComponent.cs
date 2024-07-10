5bc37f16174f74ec428a242a5391fd8493828f95
JDtrimble <igor.mamaev1@gmail.com>
2024-07-11 00:39:06 +0300

    lava

---
+++ Content.Server/_Sunrise/Tiles/LavaComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.Audio;
+
+namespace Content.Server._Sunrise.Tiles;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class LavaComponent : Component
+{
+    [DataField] public SoundSpecifier DesintegrationSound = new SoundPathSpecifier("/Audio/Effects/lightburn.ogg");
+    [DataField] public float FireStacks = 1.25f;
+}
---
+++ Content.Server/_Sunrise/Tiles/LavaSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Shared.StepTrigger.Systems;
+
+namespace Content.Server._Sunrise.Tiles;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class LavaSystem : EntitySystem
+{
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<LavaComponent, StepTriggeredOnEvent>(OnStepTriggered);
+        SubscribeLocalEvent<LavaComponent, StepTriggerAttemptEvent>(OnStepTriggerAttempt);
+    }
+
+    private void OnStepTriggerAttempt(EntityUid uid, LavaComponent component, ref StepTriggerAttemptEvent args)
+    {
+        if (!HasComp<FlammableComponent>(args.Tripper))
+            return;
+        args.Continue = true;
+    }
+
+    private void OnStepTriggered(EntityUid uid, LavaComponent component, ref StepTriggeredOnEvent args)
+    {
+        if (!TryComp<FlammableComponent>(args.Tripper, out var flammableComponent))
+            return;
+
+        var num = flammableComponent.FireStacks == 0.0 ? 5f : 1f;
+        _flammable.AdjustFireStacks(args.Tripper, component.FireStacks * num, flammableComponent);
+        _flammable.Ignite(args.Tripper, uid, flammableComponent);
+    }
+}
